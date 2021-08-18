59645304d491808c90485cb2423f8fadb745af88
MonteChristo <edgar.r.knapp@gmail.com>
2021-08-18 08:19:25 +0200

    seems to work

--- ODataDemo/Startup.cs
+++ ODataDemo/Startup.cs
@@ -1,13 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ODataDemo.Models;
 
 namespace ODataDemo
 {
@@ -24,6 +20,7 @@ namespace ODataDemo
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddControllersWithViews();
+      services.AddDbContext<ODataContext>();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
